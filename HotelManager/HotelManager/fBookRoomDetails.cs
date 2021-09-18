83d22c1e2b62848acbaa796831d24dff9e548134
thientanhuynh189 <tanhtctk42@gmail.com>
2021-09-18 03:49:05 -0700

    Done properties of fBookRoomDetails

--- HotelManager/HotelManager/fBookRoomDetails.cs
+++ HotelManager/HotelManager/fBookRoomDetails.cs
@@ -23,11 +23,44 @@ namespace HotelManager
             InitializeComponent();
             LoadRoomType();
             LoadCustomerType();
+            LoadData();
+            LoadDays();
+            txbIDBookRoom.Text = _idBookRoom.ToString();
+        }
+
+        private void LoadDays()
+        {
+            txbDays.Text = (dpkDateCheckOut.Value.Date - dpkDateCheckIn.Value.Date).Days.ToString();
+        }
+
+        private void LoadData()
+        {
+            DataRow data = BookRoomDAO.Instance.ShowBookRoomInfo(idBookRoom);
+            dpkDateCheckIn.Value = (DateTime)data["DateCheckIn"];
+            dpkDateCheckOut.Value = (DateTime)data["DateCheckOut"];
+
+            cbRoomType.Text = RoomTypeDAO.Instance.GetRoomTypeByIdBookRoom(idBookRoom).Name;
+
+            getInfoByIdCard(idCard);
+        }
+
+        private void getInfoByIdCard(string idCard)
+        {
+            CustomerDTO customer = CustomerDAO.Instance.GetInfoByIdCard(idCard);
+            txbIDCard.Text = customer.IdCard.ToString();
+            txbFullName.Text = customer.Name;
+            txbAddress.Text = customer.Address;
+            dpkDateOfBirth.Value = customer.DateOfBirth;
+            cbSex.Text = customer.Sex;
+            txbPhoneNumber.Text = customer.PhoneNumber.ToString();
+            cbNationality.Text = customer.Nationality;
+            cbCustomerType.Text = CustomerTypeDAO.Instance.GetNameByIdCard(idCard);
         }
 
         private void LoadCustomerType()
         {
             cbCustomerType.DataSource = CustomerTypeDAO.Instance.LoadListCustomerType();
+            cbCustomerType.DisplayMember = "Name";
         }
 
         private void LoadRoomType()
